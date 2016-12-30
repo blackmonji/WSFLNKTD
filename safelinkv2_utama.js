20acb2cd07ffd3ae589b58b356588a17c48d6c83
blackmonji <masamune.arifianto@gmail.com>
2016-12-30 09:10:07 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -22,9 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="live-your-golden-years-in-daily-grind.html"}
-if(x=="2"){xxx="tips-to-obtain-healthy-and-balanced-and.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://websafelinktodownload.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="tips-to-obtain-healthy-and-balanced-and.html"}a_to_ve[i].href="https://websafelinktodownload.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
